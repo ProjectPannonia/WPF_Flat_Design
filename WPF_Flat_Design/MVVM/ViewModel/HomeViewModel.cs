ef853db001d8f41aa5666d4a763919fa42bac989
Letenyei Ádám <letenyei.adam@outlook.hu>
2023-11-12 14:05:46 +0100

    HomeView created and implemented

---
+++ WPF_Flat_Design/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Flat_Design.Core;
+
+namespace WPF_Flat_Design.MVVM.ViewModel
+{
+    public class HomeViewModel : ObservableObject
+    {
+        public HomeViewModel() 
+        {
+        
+        }
+    }
+}
