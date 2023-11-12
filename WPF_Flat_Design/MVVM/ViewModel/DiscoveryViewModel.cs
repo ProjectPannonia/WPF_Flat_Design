344b45f2a3247f82212fd4fb33033c1a1758f047
Letenyei Ádám <letenyei.adam@outlook.hu>
2023-11-12 14:21:44 +0100

    Dicovery View and ViewModel created

---
+++ WPF_Flat_Design/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Flat_Design.MVVM.ViewModel
+{
+    public class DiscoveryViewModel
+    {
+        public DiscoveryViewModel() 
+        {
+        
+        }
+    }
+}
--- WPF_Flat_Design/MVVM/ViewModel/MainViewModel.cs
+++ WPF_Flat_Design/MVVM/ViewModel/MainViewModel.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WPF_Flat_Design.Core;
+﻿using WPF_Flat_Design.Core;
 
 namespace WPF_Flat_Design.MVVM.ViewModel
 {
     public class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainViewModel() 
         {
-        
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
         }
     }
 }
